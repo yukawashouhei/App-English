969a749b6528c88ae55031b0480b75a170aa6edf
Shohei <yukawa.shouhei@gmail.com>
2025-08-02 10:52:56 +0900

    Initial Commit

---
+++ Swift English/Swift_EnglishApp.swift
@@ -0,0 +1,17 @@
+//
+//  Swift_EnglishApp.swift
+//  Swift English
+//
+//  Created by 湯川昇平 on 2025/08/02.
+//
+
+import SwiftUI
+
+@main
+struct Swift_EnglishApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
